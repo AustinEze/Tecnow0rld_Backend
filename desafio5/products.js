5bde6bd66a5234034524a467a688b1fb8776d16b
nezek159 <nezek159@gmail.com>
2022-09-29 19:43:33 -0300

    desafio 5 backend (pug)

---
+++ desafio5/products.js
@@ -0,0 +1,70 @@
+const Container = require('./index.js');
+const express = require('express');
+const {
+    Router
+} = express;
+
+const router = Router();
+
+const container = new Container('./data.json');
+
+router.post('/', async (req, res) => {
+    const saveProduct = req.body;
+    await container.save(saveProduct);
+    res.status(201).redirect('/api/products');
+})
+
+router.get('/products', async (req, res) => {
+    res.render('table', {
+        products: await container.getAll(),
+        message: "No products found"
+    });
+})
+
+router.get('/products/:id', async (req, res) => {
+    let {
+        id
+    } = req.params;
+
+    id = parseInt(id);
+
+    const product = await container.getById(id);
+    if (product === 0) {
+        res.status(400).send({
+            error: 'Product not found.'
+        });
+    } else {
+        res.status(200).send(product);
+    }
+})
+
+
+router.put('/products/:id', async (req, res) => {
+    let {
+        id
+    } = req.params;
+
+    id = parseInt(id);
+
+    let product = await container.getById(id);
+
+    if (product === 0) {
+        res.status(400).send({
+            error: 'Product not found.'
+        });
+    } else {
+        product = req.body;
+        res.status(201).send(await container.modifyById(id, product));
+    }
+})
+
+router.delete('/products/:id', async (req, res) => {
+    let {
+        id
+    } = req.params;
+
+    id = parseInt(id);
+    res.status(201).send(await container.deleteById(id));
+})
+
+module.exports = router;
---
+++ desafio5/server.js
@@ -0,0 +1,22 @@
+const products = require('./products');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use('/api/', products);
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+    console.log('Server started on port', server.address().port);
+}).on('error', (err) => console.log(err));
